118f86d4a2e5bc7b4a3def058055dcddc4559d19
Andsemandse <andr90m3@stud.kea.dk>
2023-06-20 16:14:56 +0200

    forsøg igen

--- src/main/java/com/example/demo/tyr.java
+++ src/main/java/com/example/demo/tyr.java
@@ -11,10 +11,10 @@ public class tyr {
 
             if (number == 1) {
                 System.out.println("1");
-            } else if (number == 2) {
-                System.out.println("2");
-            } else if (number == 4) {
-                System.out.println("4");
+            } else if (number == 3) {
+                System.out.println("3");
+            } else if (number == 5) {
+                System.out.println("5");
             } else {
                 System.out.println("Ingen af delene");
             }
